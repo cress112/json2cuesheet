261b0bd2b3a693f33c48116574a6d0c8e816ca67
cress112 <m210d498a75.festa@icloud.com>
2026-03-01 15:23:33 +0900

    fix: return code is 0 or 1

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
+use std::process::ExitCode;
+
 use json2cuesheet::app::usecases::build_and_save_file;
 use json2cuesheet::infra::app::CliParser;
 
-fn main() -> Result<(), String> {
+fn main() -> ExitCode {
     let args_getter = CliParser::new();
-    build_and_save_file(args_getter)
+    match build_and_save_file(args_getter) {
+        Ok(_) => ExitCode::SUCCESS,
+        Err(e) => {
+            println!("{}", e);
+            return ExitCode::FAILURE;
+        }
+    }
 }
